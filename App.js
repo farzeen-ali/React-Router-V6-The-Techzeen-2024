065d18add5339afdfbbceec711773daee334c3bd
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-02-24 22:25:32 +0500

    App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './MyRouters/Home';
 import About from './MyRouters/About';
+import Navbar from './MyRouters/Navbar';
 import './App.css';
 function App() {
     return (
       <div className='App'>
         <h1>This is my App JS File</h1>
         <BrowserRouter>
+        <Navbar />
           <Routes>
             <Route path='/home' element={<Home />} />
             <Route path='/about' element={<About />} />
