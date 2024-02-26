d79e7358814995615a3d3ce19140d9e2670089d0
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-02-26 22:09:53 +0500

    App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './MyRouters/Home';
 import About from './MyRouters/About';
 import Navbar from './MyRouters/Navbar';
+import Navbar from './MyRouters/Employee';
 import './App.css';
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/home' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/employee/:name/:age' element={<Employee />} />
           </Routes>
         </BrowserRouter>
       </div>
