ebd539b9b22f2583d9c54a4228de4edd7fcc3a2e
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-02-27 22:55:15 +0500

    Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-     <ul>
+{/*      <ul>
         <li><Link to="/home">Home</Link></li>
         <li><Link to="/about">About</Link></li>
+     </ul>  */}
+
+      <ul className='nav'>
+        <li><NavLink className='nav-link' style={({isActive}) => {return {backgroundColor: isActive ? 'aqua' : 'yellow'}}} to="/">Home</NavLink></li>
+        <li><NavLink className='nav-link' style={({isActive}) => {return {backgroundColor: isActive ? 'aqua' : 'yellow'}}} to="/about">About</NavLink></li>
+        <li><NavLink className='nav-link' style={({isActive}) => {return {backgroundColor: isActive ? 'aqua' : 'yellow'}}} to="/contact">Contact</NavLink></li>
      </ul> 
     </>
   );
