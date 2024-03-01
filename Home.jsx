9616871ca37aea6ed224a8a1abaf6b39c76404c8
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-03-01 22:04:00 +0500

    Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Home = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <>
-     <h2>Home</h2> 
+     <h2>Home</h2>
     </>
   );
 }
