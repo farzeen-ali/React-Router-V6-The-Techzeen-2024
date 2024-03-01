4fe270793acb7c859c1b08a5639d373c6089d63b
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-03-01 22:03:11 +0500

    About.jsx

--- About.jsx
+++ About.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const About = () => {
   return (
     <>
-     <h2>About</h2> 
+     <h2>About</h2>
+     <li><Link to="/employee/farzeen/23" state={{name:'farzeen ali', age:23}}>Farzeen</Link></li>
+      <li><Link to="/employee/huzaifa/22">Huzaifa</Link></li>
     </>
   );
 }
