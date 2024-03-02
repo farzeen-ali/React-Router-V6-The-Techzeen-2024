b3aede359a46ec8677e8e2a49c411094902de1a0
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-03-02 22:55:34 +0500

    App.js

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './MyRouters/Home';
-import About from './MyRouters/About';
-import Navbar from './MyRouters/Navbar';
-import Navbar from './MyRouters/Employee';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './MyRouters/Home'
+import About from './MyRouters/About'
 import './App.css';
+import Navbar from './MyRouters/Navbar';
+import Employee from './MyRouters/Employee';
+import Contact from './MyRouters/Contact';
+import Search from './MyRouters/Search';
+import Company from './MyRouters/Company';
+import Admin from './MyRouters/Admin';
+import Faculty from './MyRouters/Faculty';
+import Student from './MyRouters/Student';
 function App() {
     return (
       <div className='App'>
-        <h1>This is my App JS File</h1>
         <BrowserRouter>
-        <Navbar />
+          <Navbar />
           <Routes>
-            <Route path='/home' element={<Home />} />
+            <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/company/' element={<Company />}>
+              <Route path='admin' element={<Admin />} />
+              <Route path='faculty' element={<Faculty />} />
+              <Route path='student' element={<Student />} />
+            </Route>
             <Route path='/employee/:name/:age' element={<Employee />} />
+            <Route path='/*' element={<Navigate to='/' />} />
           </Routes>
         </BrowserRouter>
       </div>
