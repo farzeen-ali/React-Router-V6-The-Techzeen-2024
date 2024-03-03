c890a067b790ecbfa5f0a88bf44fff1c2ed8b78e
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-03-03 22:57:21 +0500

    Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 const Home = () => {
-  const location = useLocation();
-  console.log(location);
+  // const location = useLocation();
+  // console.log(location);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem('login');
+    navigate('/login')
+  }
   return (
     <>
      <h2>Home</h2>
+     <button onClick={handleLogout}>Logout</button>
     </>
   );
 }
