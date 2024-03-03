7e38fb3c41a5a4f1e191718d9fd8e55c8ce5e2fb
Farzeen Ali <90315008+farzeen-ali@users.noreply.github.com>
2024-03-03 22:55:01 +0500

    \App.js

--- App.js
+++ App.js
@@ -10,15 +10,19 @@ import Company from './MyRouters/Company';
 import Admin from './MyRouters/Admin';
 import Faculty from './MyRouters/Faculty';
 import Student from './MyRouters/Student';
+import Login from './MyRouters/Login';
+import Protected from './MyRouters/Protected';
 function App() {
     return (
       <div className='App'>
         <BrowserRouter>
           <Navbar />
           <Routes>
-            <Route path='/' element={<Home />} />
+            {/* <Route path='/' element={<Home />} /> */}
+            <Route path='/' element={<Protected Component={Home} />} />
             <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
+            {/* <Route path='/contact' element={<Contact />} /> */}
+            <Route path='/contact' element={<Protected Component={Contact} />} />
             <Route path='/search' element={<Search />} />
             <Route path='/company/' element={<Company />}>
               <Route path='admin' element={<Admin />} />
@@ -26,6 +30,7 @@ function App() {
               <Route path='student' element={<Student />} />
             </Route>
             <Route path='/employee/:name/:age' element={<Employee />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/*' element={<Navigate to='/' />} />
           </Routes>
         </BrowserRouter>
